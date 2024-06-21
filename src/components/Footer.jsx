f4be6f074e1ca51a5bfb3fc92ff070804c0458c5
akhil.dumbre <akhildumbre1411@gmail.com>
2024-06-22 00:04:37 +0530

    sample project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/@TeamWCPxNoFeaR" target={"blank"}>
+            YouTube
+          </a>
+          <a href="https://www.instagram.com/akhillll._14" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/akhil0714" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
